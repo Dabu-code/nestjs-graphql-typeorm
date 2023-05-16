307162d2c1e445ff9d11b6bb61bf742491952841
DannyJoel05 <dannyjoel98@hotmail.com>
2023-05-15 21:55:19 -0500

    Incorporación de @Subscription para reactividad

--- src/modules/post/infrastruture/resource/resolver/post.resolver.ts
+++ src/modules/post/infrastruture/resource/resolver/post.resolver.ts
@@ -1,15 +1,19 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 
 import { postSchema } from '../../schema/post.schema';
 import { InternalServerErrorException } from '@nestjs/common';
 import { createPostDto } from '../../dto/create-post.dto';
 import { postService } from '../service/post.service';
 import { updatePostDto } from '../../dto/update-post.dto';
+import { PubSub } from 'graphql-subscriptions';
 
 @Resolver()
 export class postResolver {
+    private pubSub: PubSub;
 
-    constructor(private readonly post_service: postService) { }
+    constructor(private readonly post_service: postService) {
+        this.pubSub = new PubSub();
+    }
 
     @Query(() => postSchema)
     public async post(@Args("uuid") uuid: string): Promise<postSchema> {
@@ -32,7 +36,10 @@ export class postResolver {
     @Mutation(() => postSchema)
     public async create_post(@Args("create_post") value: createPostDto): Promise<postSchema> {
         try {
-            return await this.post_service.create(value);
+            const created: postSchema = await this.post_service.create(value);
+            this.pubSub.publish("post_event", { post_event: created })
+            return created;
+
         } catch (e: any) {
             throw new InternalServerErrorException(e.message);
         }
@@ -55,5 +62,10 @@ export class postResolver {
         } catch (e: any) {
             throw new InternalServerErrorException(e.message);
         }
+    }
+
+    @Subscription(() => postSchema)
+    public post_event(): AsyncIterator<postSchema> {
+        return this.pubSub.asyncIterator<postSchema>("post_event")
     }
 }
