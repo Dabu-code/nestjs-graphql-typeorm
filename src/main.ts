d0ef849214a2a7bbfb2c4d9d52d04d54306e5381
DannyJoel05 <dannyjoel98@hotmail.com>
2023-05-15 22:03:51 -0500

    Integración GRPC

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport:Transport.GRPC,
+    options:{
+      url:'0.0.0.0:50051',
+      package:"post",
+      protoPath: join(process.cwd(),"src/modules/post/infrastruture/proto/post.proto")
+    }
+  })
   await app.listen(3000);
 }
 bootstrap();
